38c71c723f76ff7c70fc2b8a49d79c6548e9876a
DeF4lt-Cap10N <captainakash1675@gmail.com>
2025-08-06 19:26:53 +0530

    Init frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/Button";
+import { ShareIcon } from "./icons/ShareIcon";
+const App = () => {
+  return (
+    <div >
+      <Button
+        variant="secondary"
+        text="Submit"
+        onClick={() => console.log("Clicked")}
+        loading={false}
+        startIcon={<ShareIcon/>}
+      />
+      <Button variant="primary" text="Click Me" />
+    </div>
+  );
+};
+
+export default App;
